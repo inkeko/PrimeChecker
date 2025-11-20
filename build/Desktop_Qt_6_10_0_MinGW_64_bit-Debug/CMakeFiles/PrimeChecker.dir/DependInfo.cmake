09fbd2b18a7907e720658150309a4f95019fc0b7
Földi Csaba <50557675+inkeko@users.noreply.github.com>
2025-11-20 09:48:17 +0100

    Initial commit

---
+++ build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/CMakeFiles/PrimeChecker.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "PrimeChecker_autogen/timestamp" "custom" "PrimeChecker_autogen/deps"
+  "D:/cpp_gyakorlas/PrimeChecker/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/PrimeChecker_autogen/mocs_compilation.cpp" "CMakeFiles/PrimeChecker.dir/PrimeChecker_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/PrimeChecker.dir/PrimeChecker_autogen/mocs_compilation.cpp.obj.d"
+  "D:/cpp_gyakorlas/PrimeChecker/main.cpp" "CMakeFiles/PrimeChecker.dir/main.cpp.obj" "gcc" "CMakeFiles/PrimeChecker.dir/main.cpp.obj.d"
+  "D:/cpp_gyakorlas/PrimeChecker/mainwindow.cpp" "CMakeFiles/PrimeChecker.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/PrimeChecker.dir/mainwindow.cpp.obj.d"
+  "D:/cpp_gyakorlas/PrimeChecker/resultform.cpp" "CMakeFiles/PrimeChecker.dir/resultform.cpp.obj" "gcc" "CMakeFiles/PrimeChecker.dir/resultform.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/CMakeFiles/PrimeChecker.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrimeChecker.dir/PrimeChecker_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/PrimeChecker.dir/PrimeChecker_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/PrimeChecker.dir/main.cpp.obj"
+  "CMakeFiles/PrimeChecker.dir/main.cpp.obj.d"
+  "CMakeFiles/PrimeChecker.dir/mainwindow.cpp.obj"
+  "CMakeFiles/PrimeChecker.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/PrimeChecker.dir/resultform.cpp.obj"
+  "CMakeFiles/PrimeChecker.dir/resultform.cpp.obj.d"
+  "CMakeFiles/PrimeChecker_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/PrimeChecker_autogen.dir/ParseCache.txt"
+  "PrimeChecker_autogen"
+  "PrimeChecker.exe"
+  "PrimeChecker.exe.manifest"
+  "PrimeChecker.pdb"
+  "PrimeChecker_autogen/mocs_compilation.cpp"
+  "PrimeChecker_autogen/timestamp"
+  "libPrimeChecker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrimeChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
